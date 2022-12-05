b3304d432abcd8bd4cabb84ade1ceba8d9fa5a2e
Ognjen <radisicognjen@gmail.com>
2022-12-05 21:48:57 +0100

    Day 4 problem

---
+++ Day4/partTwo.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+
+fs.readFile("data.txt", "utf8", (err, data) => {
+	if (err) throw err;
+	let totalScore = 0;
+	data.split("\n").forEach((pair) => {
+		const firstElf = pair.split(",")[0].split("-");
+		const secondElf = pair.split(",")[1].replace("\r", "").split("-");
+
+		if (+firstElf[1] >= +secondElf[0] && +firstElf[0] <= +secondElf[1])
+			totalScore += 1;
+	});
+	console.log(totalScore);
+});
