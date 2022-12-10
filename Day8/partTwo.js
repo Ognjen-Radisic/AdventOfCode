d760d9adb5732244d8b25e584b1e655d84adf08a
Ognjen <radisicognjen@gmail.com>
2022-12-10 22:46:28 +0100

    Day8 partTwo complete

--- Day8/partTwo.js
+++ Day8/partTwo.js
@@ -11,6 +11,9 @@ fs.readFile("data.txt", "utf8", (err, data) => {
 	const visibleTreesCoordinates = {};
 	const treeMap = data.split(/\r?\n/).map((row) => row.split(""));
 
+	const height = treeMap.length - 1;
+	const width = treeMap[0].length - 1;
+
 	const lookedFromLeftToRight = treeMap.map(() => []);
 	const lookedFromTopToDown = treeMap.map(() => []);
 	const lookedFromRightToLeft = treeMap.map(() => []);
@@ -18,49 +21,93 @@ fs.readFile("data.txt", "utf8", (err, data) => {
 	const compound = treeMap.map(() => []);
 
 	// from left to right
-	for (let i = 98; i <= treeMap.length - 1; i++) {
-		for (let j = 0; j <= treeMap[0].length - 1; j++) {
-			let howMuchWeSee = 1;
-			for (let k = j; k <= treeMap[0].length - 1; k++) {
-				if (treeMap[i][j] > treeMap[i][k + 1]) {
+	for (let heightIndex = 0; heightIndex <= height; heightIndex++) {
+		for (let widthIndex = 0; widthIndex <= width; widthIndex++) {
+			let howMuchWeSee = 0;
+			for (let cur = widthIndex + 1; cur <= width; cur++) {
+				if (treeMap[heightIndex][widthIndex] > treeMap[heightIndex][cur]) {
 					howMuchWeSee += 1;
-				} else break;
+				} else if (!treeMap[heightIndex][cur]) {
+					break;
+				} else {
+					howMuchWeSee += 1;
+					break;
+				}
 			}
-			lookedFromLeftToRight[i].push(howMuchWeSee);
+			lookedFromLeftToRight[heightIndex].push(howMuchWeSee);
 		}
 	}
 	//from top to down
-	// for (let i = treeMap[0].length - 2; i <= treeMap[0].length - 1; i++) {
-	// 	for (let j = 0; j <= treeMap.length - 1; j++) {
-	// 		let howMuchWeSee = 1;
-	// 		for (let k = j; k <= treeMap.length - 1; k++) {
-	// 			if (treeMap[j][i] > treeMap[j + 1][k]) {
-	// 				howMuchWeSee += 1;
-	// 			} else break;
-	// 		}
-	// 		lookedFromTopToDown[j].push(howMuchWeSee);
-	// 	}
-	// }
-	// //from right to left
-	// for (let i = 0; i <= treeMap.length - 1; i++) {
-	// 	let biggest = -1;
-	// 	for (let j = treeMap[0].length - 1; j >= 0 - 1; j--) {
-	// 		if (+treeMap[i][j] > biggest) {
-	// 			biggest = +treeMap[i][j];
-	// 			updateMap(visibleTreesCoordinates, i, j);
-	// 		}
-	// 	}
-	// }
-	// //from down to top
-	// for (let i = treeMap[0].length - 1; i >= 0; i--) {
-	// 	let biggest = -1;
-	// 	for (let j = treeMap.length - 1; j >= 0; j--) {
-	// 		if (+treeMap[j][i] > biggest) {
-	// 			biggest = +treeMap[j][i];
-	// 			updateMap(visibleTreesCoordinates, j, i);
-	// 		}
-	// 	}
-	// }
+	for (let widthInd = 0; widthInd <= width; widthInd++) {
+		for (let heightIndex = 0; heightIndex <= height; heightIndex++) {
+			let howMuchWeSee = 0;
+			for (let cur = heightIndex + 1; cur <= height; cur++) {
+				if (treeMap[heightIndex][widthInd] > treeMap[cur][widthInd]) {
+					howMuchWeSee += 1;
+				} else if (!treeMap[cur][widthInd]) {
+					break;
+				} else {
+					howMuchWeSee += 1;
+					break;
+				}
+			}
+			lookedFromTopToDown[heightIndex].push(howMuchWeSee);
+		}
+	}
+	//from right to left
+	for (let heightIndex = height; heightIndex >= 0; heightIndex--) {
+		for (let widthIndex = width; widthIndex >= 0; widthIndex--) {
+			let howMuchWeSee = 0;
+			for (let cur = widthIndex - 1; cur >= 0; cur--) {
+				if (treeMap[heightIndex][widthIndex] > treeMap[heightIndex][cur]) {
+					howMuchWeSee += 1;
+				} else if (!treeMap[heightIndex][cur]) {
+					break;
+				} else {
+					howMuchWeSee += 1;
+					break;
+				}
+			}
+			lookedFromRightToLeft[heightIndex].unshift(howMuchWeSee);
+		}
+	}
+	//from down to top
+	for (let widthInd = 0; widthInd <= width; widthInd++) {
+		for (let heightIndex = height; heightIndex >= 0; heightIndex--) {
+			let howMuchWeSee = 0;
+			for (let cur = heightIndex - 1; cur >= 0; cur--) {
+				if (treeMap[heightIndex][widthInd] > treeMap[cur][widthInd]) {
+					howMuchWeSee += 1;
+				} else if (!treeMap[cur][widthInd]) {
+					break;
+				} else {
+					howMuchWeSee += 1;
+					break;
+				}
+			}
+			lookedFromDownToTop[heightIndex].push(howMuchWeSee);
+		}
+	}
 
-	console.log(lookedFromLeftToRight);
+	// make compound
+	for (let heightIndex = 0; heightIndex <= height; heightIndex++) {
+		for (let widthIndex = 0; widthIndex <= width; widthIndex++) {
+			const compoundValueOfSingleTree =
+				lookedFromLeftToRight[heightIndex][widthIndex] *
+				lookedFromTopToDown[heightIndex][widthIndex] *
+				lookedFromRightToLeft[heightIndex][widthIndex] *
+				lookedFromDownToTop[heightIndex][widthIndex];
+			compound[heightIndex].push(compoundValueOfSingleTree);
+		}
+	}
+
+	let bestTree = 0;
+	for (let heightIndex = 0; heightIndex <= height; heightIndex++) {
+		for (let widthIndex = 0; widthIndex <= width; widthIndex++) {
+			if (compound[heightIndex][widthIndex] > bestTree)
+				bestTree = compound[heightIndex][widthIndex];
+		}
+	}
+
+	console.log(bestTree);
 });
